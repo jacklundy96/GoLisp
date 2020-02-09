7bce93ee8815e5680a26d9dcc06a324aa8ff54ef
Jack Lundy <jacklundy@hotmail.co.uk>
2020-02-09 19:53:01 +0000

    Update initial token definitions

--- Interpreter.go
+++ Interpreter.go
@@ -6,10 +6,17 @@ const (
 	LPAREN Token = 1
 	RPAREN Token = 2
 	NUMBER Token = 3
-	SYMBOL Token = 4
+	ATOM Token = 4
+	LIST Token = 5 
+	SYMBOL Token = 6
+	ILLEGAL Token = 7
+	EOF Token = 8
 )
-
 
 func main() {
 
 }
+//Convert input string into tokens
+func tokenize(inputString string) string {
+	return ""
+} 
